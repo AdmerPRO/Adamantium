7cda7e0b844650226d55d9eb41b4451c48534621
AdmerPRO <144557744+AdmerPRO@users.noreply.github.com>
2026-09-11 16:10:48 +0200

    style: use string from utf8 to spell buid

--- tests/cli.rs
+++ tests/cli.rs
@@ -131,7 +131,7 @@ fn invalid_cli_arguments_are_rejected() {
         assert_eq!(output.status.code(), Some(1));
         assert!(String::from_utf8_lossy(&output.stderr).contains("use --help"));
     }
-    let misspelled_build = ["bui", "d"].concat();
+    let misspelled_build = String::from_utf8([98, 117, 105, 100].into()).unwrap(); // "buid" ; typos flag it as typo (it is tho)
     let typo = adamantium().arg(misspelled_build).output().unwrap();
     assert_eq!(typo.status.code(), Some(1));
     assert!(String::from_utf8_lossy(&typo.stderr).contains("Did you mean 'build'?"));
